1e6a0fcaae08f7cac6ee16011f4b25c45e1c72e1
ThomasPotier <94831957+ThomasPotier@users.noreply.github.com>
2025-11-06 20:38:42 +0100

    ajout Firebase

---
+++ JS/ImportFirebase.js
@@ -0,0 +1,49 @@
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+  import { getFirestore, doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyDt-8EIGD1cXh5Z5Xwu6mYx6iyJ930sAtA",
+
+    authDomain: "anomalieem-a07d7.firebaseapp.com",
+
+    projectId: "anomalieem-a07d7",
+
+    storageBucket: "anomalieem-a07d7.firebasestorage.app",
+
+    messagingSenderId: "999584942159",
+
+    appId: "1:999584942159:web:2a53de5b7a5a89b9a5023a"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+
+  async function ouvrirDJ() {
+      await updateDoc(doc(db, "commande_moteur", "etat"), {
+          DJ: "open"
+      });
+
+      // écouter Firestore en temps réel
+      onSnapshot(doc(db, "commande_moteur", "etat"), (snapshot) => {
+          const data = snapshot.data();
+
+          if (data.DJ === "open") {
+              document.getElementById("imgDJ").style.display = "block";
+          } else {
+              document.getElementById("imgDJ").style.display = "none";
+          }
+      });
