1c49e8f50aec4b128e9ade034dd8e33a18d63425
ThomasPotier <94831957+ThomasPotier@users.noreply.github.com>
2025-11-06 22:34:38 +0100

    config firebase

--- JS/commandeFirebase.js
+++ JS/commandeFirebase.js
@@ -5,10 +5,20 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "...",
-    authDomain: "...",
-    databaseURL: "...",
-    projectId: "...",
+  apiKey: "AIzaSyDt-8EIGD1cXh5Z5Xwu6mYx6iyJ930sAtA",
+
+  authDomain: "anomalieem-a07d7.firebaseapp.com",
+
+  databaseURL: "https://anomalieem-a07d7-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "anomalieem-a07d7",
+
+  storageBucket: "anomalieem-a07d7.firebasestorage.app",
+
+  messagingSenderId: "999584942159",
+
+  appId: "1:999584942159:web:2a53de5b7a5a89b9a5023a"
+
 };
 
 const app = initializeApp(firebaseConfig);
