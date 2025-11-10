84204f2f2ff8fe69978e9b72b88705094c742a74
ThomasPotier <94831957+ThomasPotier@users.noreply.github.com>
2025-11-10 16:36:18 +0100

    modif min Uligne

--- JS/commandeFirebase.js
+++ JS/commandeFirebase.js
@@ -179,7 +179,7 @@ document.querySelector('button[name="UMonoJ"]').addEventListener("click", () => 
 });
 
 document.querySelector('button[name="UMonoZ"]').addEventListener("click", () => {
-    sliderMono.value = 10;
+    sliderMono.value = 8;
     exclusiviteTension("mono");
     envoyerFirebaseTension();
 });
@@ -198,7 +198,7 @@ document.querySelector('button[name="UContinuJ"]').addEventListener("click", () 
 });
 
 document.querySelector('button[name="UContinuZ"]').addEventListener("click", () => {
-    sliderContinue.value = 10;
+    sliderContinue.value = 8;
     exclusiviteTension("continue");
     envoyerFirebaseTension();
 });
